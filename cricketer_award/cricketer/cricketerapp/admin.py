febb1860a3b2ef921ed467168725c4f1d0f92cba
georgesabu555 <147422723+georgesabu555@users.noreply.github.com>
2023-10-11 10:59:38 +0530

    Delete cricketer_award directory

--- cricketer_award/cricketer/cricketerapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from cricketerapp.models import Cricketer
-
-# Register your models here.
-admin.site.register(Cricketer)
--- cricketer_award/cricketer/cricketerapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Cricketer
-
-class CricketerForm(forms.ModelForm):
-    class Meta:
-        model=Cricketer
-        fields=['name','desc','year','img']
